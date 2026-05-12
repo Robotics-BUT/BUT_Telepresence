1914fba11ae4cd5c84a53eaa1ddc443444941df3
Stanislav Svědiroh <standa.svediroh@gmail.com>
2026-05-12 22:38:07 +0200

    Formatting changes

--- streaming_driver/include/pipelines.h
+++ streaming_driver/include/pipelines.h
@@ -48,10 +48,9 @@ inline std::ostringstream GetH264StreamingPipeline(const StreamingConfig &stream
 
     std::ostringstream oss;
     oss << "nvarguscamerasrc aeantibanding=AeAntibandingMode_Off ee-mode=EdgeEnhancement_Off tnr-mode=NoiseReduction_Off saturation=1.2 exposuretimerange=\"4000000 4000000\" sensor-id=" << sensorId
-        << " ! " << "video/x-raw(memory:NVMM),width=(int)" << streamingConfig.horizontalResolution << ",height=(int)" << streamingConfig.verticalResolution
-        << ",framerate=(fraction)80/1,format=(string)NV12"
-	<< " ! identity name=camsrc_ident"
-	<< " ! nvvidconv flip-method=vertical-flip"
+        << " ! " << "video/x-raw(memory:NVMM),width=(int)" << streamingConfig.horizontalResolution << ",height=(int)" << streamingConfig.verticalResolution << ",framerate=(fraction)80/1,format=(string)NV12"
+	    << " ! identity name=camsrc_ident"
+	    << " ! nvvidconv flip-method=vertical-flip"
         << " ! identity name=vidconv_ident"
         << " ! videorate drop-only=true"
         << " ! video/x-raw(memory:NVMM),framerate=" << streamingConfig.fps << "/1"
@@ -68,11 +67,12 @@ inline std::ostringstream GetH265StreamingPipeline(const StreamingConfig &stream
 
     std::ostringstream oss;
     oss << "nvarguscamerasrc aeantibanding=AeAntibandingMode_Off ee-mode=EdgeEnhancement_Off tnr-mode=NoiseReduction_Off saturation=1.2 exposuretimerange=\"4000000 4000000\" sensor-id=" << sensorId
-        << " ! " << "video/x-raw(memory:NVMM),width=(int)" << streamingConfig.horizontalResolution << ",height=(int)" << streamingConfig.verticalResolution
-        << ",framerate=(fraction)" << streamingConfig.fps << "/1,format=(string)NV12"
-	<< " ! identity name=camsrc_ident"
-	<< " ! nvvidconv flip-method=vertical-flip"
+        << " ! " << "video/x-raw(memory:NVMM),width=(int)" << streamingConfig.horizontalResolution << ",height=(int)" << streamingConfig.verticalResolution << ",framerate=(fraction)80/1,format=(string)NV12"
+	    << " ! identity name=camsrc_ident"
+	    << " ! nvvidconv flip-method=vertical-flip"
         << " ! identity name=vidconv_ident"
+        << " ! videorate drop-only=true"
+        << " ! video/x-raw(memory:NVMM),framerate=" << streamingConfig.fps << "/1"
         << " ! nvv4l2h265enc name=encoder insert-sps-pps=1 iframeinterval=10 bitrate=" << streamingConfig.bitrate << " preset-level=1"
         << " ! identity name=enc_ident"
         << " ! rtph265pay mtu=1300 config-interval=1 pt=96"
